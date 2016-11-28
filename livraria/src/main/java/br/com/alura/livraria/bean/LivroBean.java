9c65a3c724fcc81c5857d6c5e9eb1fdf8b69c321
Domenico Schettini Filho <domenicoschettini@gmail.com>
2016-11-28 07:51:11 -0200

    Projeto Web para o Curso de CDI da Alura

---
+++ livraria/src/main/java/br/com/alura/livraria/bean/LivroBean.java
@@ -0,0 +1,130 @@
+package br.com.alura.livraria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+
+import br.com.alura.alura_lib.dao.DAO;
+import br.com.alura.alura_lib.jsf.annotation.ViewModel;
+import br.com.alura.alura_lib.tx.annotation.Transacional;
+import br.com.alura.livraria.modelo.Autor;
+import br.com.alura.livraria.modelo.Livro;
+
+@ViewModel
+public class LivroBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Livro livro = new Livro();
+
+	private Integer autorId;
+
+	private List<Livro> livros;
+
+	private DAO<Livro, Integer> livroDao;
+
+	private DAO<Autor, Integer> autorDao;
+	
+	@Inject
+	public LivroBean(DAO<Livro, Integer> livroDao, DAO<Autor, Integer> autorDao){
+		this.livroDao = livroDao;
+		this.autorDao = autorDao;
+	}
+	
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public List<Livro> getLivros() {
+		DAO<Livro, Integer> dao = livroDao;
+		if(this.livros == null) {
+			this.livros = dao.listaTodos();
+		}
+		return livros;
+	}
+
+	public List<Autor> getAutores() {
+		return autorDao.listaTodos();
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public void carregarLivroPelaId() {
+		this.livro = livroDao.buscaPorId(this.livro.getId()); 
+	}
+	
+	public void gravarAutor() {
+		Autor autor = autorDao.buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+		System.out.println("Escrito por: " + autor.getNome());
+	}
+	
+	@Transacional
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			FacesContext.getCurrentInstance().addMessage("autor",
+					new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
+		}
+
+		DAO<Livro, Integer> dao = livroDao;
+		if(this.livro.getId() == null) {
+			dao.adiciona(this.livro);
+			this.livros = dao.listaTodos();
+		} else {
+			dao.atualiza(this.livro);
+		}
+
+		this.livro = new Livro();
+	}
+	
+	@Transacional
+	public void remover(Livro livro) {
+		System.out.println("Removendo livro");
+		DAO<Livro, Integer> dao = livroDao;
+		dao.remove(livro);
+		this.livros = dao.listaTodos();
+	}
+	
+	public void removerAutorDoLivro(Autor autor) {
+		this.livro.removeAutor(autor);
+	}
+	
+	public void carregar(Livro livro) {
+		System.out.println("Carregando livro");
+		this.livro = livro;
+	}
+	
+	public String formAutor() {
+		System.out.println("Chamanda do formulário do Autor.");
+		return "autor?faces-redirect=true";
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component,
+			Object value) throws ValidatorException {
+
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage(
+					"ISBN deveria começar com 1"));
+		}
+
+	}
+}
